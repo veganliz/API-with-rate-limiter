c49528bc0f02a788ac93258507f08c439a288b52
elizabeth <elizabeth.bourke@intercom.io>
2019-08-02 20:27:03 +0100

    change number of calls allowed to API per second

--- config/initializers/rack_attack.rb
+++ config/initializers/rack_attack.rb
@@ -1,7 +1,7 @@
 class Rack::Attack
   Rack::Attack.cache.store = ActiveSupport::Cache::MemoryStore.new # defaults to Rails.cache
 
-throttle("req/ip", limit: 2, period: 10) do |req|
+throttle("req/ip", limit: 3, period: 10) do |req|
   req.ip
 end
 
