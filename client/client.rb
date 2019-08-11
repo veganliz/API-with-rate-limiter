f82e94744677b5011b490c02a8056e92da23de6b
elizabeth <elizabeth.bourke@intercom.io>
2019-08-11 15:32:48 +0100

    tidying up output of seconds

--- client/client.rb
+++ client/client.rb
@@ -15,17 +15,18 @@ class ExponentialBackOff
       puts response.code
       puts response
 
-
 	# If not rate limited, break out of while loop and continue with the rest of the code
 	     if response.code != 429
          puts 'success'
 		   break
 
 	# If rate limited, wait and try again
-      else puts "You've hit our rate limit. Please wait"
+       else puts "You've hit our rate limit. Please wait"
 	       sleep 2 ** retries
          number = 2 ** retries
-         puts number
+  # Print total number of seconds wait before code will retry
+         puts "Seconds before retry:  " + number.to_s
+  # Print seconds to the console as the time passes
          print_seconds(number)
          retries = retries + 1
        end
